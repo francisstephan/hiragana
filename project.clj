37d1084efdf3f1b2254b12d8deb24fe49eae2719
Francis Stephan <francis.stephan@free.fr>
2015-10-12 10:59:41 +0000

    initial version 2015/10/09

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hiragana "0.1.0-SNAPSHOT"
+  :description "Generate hiragana from latin keyboard"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.4.1"]]
+  :main hiragana.core)
